72a4fb746b8a3c8463fc968db837c7be3f9701a8
Samuel Wang <samuelzixuan.wang1215@gmail.com>
2025-08-06 13:30:48 -0700

    Initial Commit

---
+++ ISO.Cube/ISO_CubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISO_CubeApp.swift
+//  ISO.Cube
+//
+//  Created by Samuel Wang on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ISO_CubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
